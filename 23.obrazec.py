b0cfc400d25719b76e96986daaed7d21b9f2938b
bulvardneutral <bulvardneutral@gmail.com>
2024-05-18 14:26:51 +0200

    Update and rename 23.obrazec to 23.obrazec.py

---
+++ 23.obrazec.py
@@ -0,0 +1,23 @@
+n = 5 
+
+for i in range(1,n+1):
+    if i == 1:
+        print(' ' * (n - i) + '*')
+    elif i == n:
+        print('*' * (2 * i - 1))
+    else:
+        print(' ' * (n - i) + '*' + ' ' * (2 * i - 3) + '*')
+print()
+
+
+n = 5
+
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*(2*i-1))
+print()
+
+n = 5
+
+for i in range(n, 0, -1):
+    print(" "*(n-i) + "*"*(2*i-1))
+
